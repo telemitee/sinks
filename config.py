31f5ce56cffc5f78a88b86d1863f8cf0461710b1
telemitee <blazeairdrop@gmail.com>
2025-08-23 19:50:49 +0545

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather, --⚠️ REQUIRED--
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7869629794:AAH7sk6TXq2Gye-Vi0nGzX_ZOxQM4EeocqM")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8391477975:AAEkwE4LsGGhFgcckc91myGO9xl9H2miwKQ")
 
 #Your API ID from my.telegram.org, --⚠️ REQUIRED--
-APP_ID = int(os.environ.get("APP_ID", "21184495"))
+APP_ID = int(os.environ.get("APP_ID", "26052197"))
 
 #Your API Hash from my.telegram.org, --⚠️ REQUIRED--
-API_HASH = os.environ.get("API_HASH", "7238819d51a5280143fc3023a2f1abed")
+API_HASH = os.environ.get("API_HASH", "ad79609ad71fe4f4bd5f9cb385ebe106")
 
 #Your db channel Id --⚠️ REQUIRED--
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002397556354"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002904301136"))
 
 #OWNER ID --⚠️ REQUIRED--
-OWNER_ID = int(os.environ.get("OWNER_ID", "7527170018"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8131593788"))
 
 #SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "https://t.me/")
@@ -26,11 +26,11 @@ SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "https://t.me/")
 PORT = os.environ.get("PORT", "8080")
 
 #Database --⚠️ REQUIRED--
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://akashz9t:akash123@cluster0.zu0io.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ardereshop:ardereshop@cluster0.gd8oqiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DB_NAME = os.environ.get("DATABASE_NAME", "heavy")
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "10"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
 PICS = (os.environ.get("PICS", "https://envs.sh/JP6.jpg")).split() #Required
